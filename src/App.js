2bfead2a9e80f20e1facf604edbe35e3b784db54
eommoonjoo <eommoonjoo@paycrux.co.kr>
2023-05-23 21:29:38 +0900

    Update: konva resize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Stage, Layer, Rect, Transformer } from "react-konva";
 
-function App() {
+const RectComponent = ({ rectProps, isSelected, onSelect, onChange }) => {
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isSelected) {
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+      <Rect
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...rectProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...rectProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={() => {
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          node.scaleX(1);
+          node.scaleY(1);
+
+          onChange({
+            ...rectProps,
+            x: node.x(),
+            y: node.y(),
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && <Transformer ref={trRef} />}
+    </React.Fragment>
+  );
+};
+
+const App = () => {
+  const [rectangles, setRectangles] = useState([]);
+  const [selectedId, selectShape] = useState(null);
+
+  const handleAddRect = () => {
+    const newRect = {
+      id: rectangles.length + 1,
+      x: Math.random() * 100,
+      y: Math.random() * 100,
+      width: 100,
+      height: 100,
+      fill: "green",
+    };
+    setRectangles([...rectangles, newRect]);
+  };
+
+  return (
+    <div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {rectangles.map((rect, i) => (
+            <RectComponent
+              key={i}
+              rectProps={rect}
+              isSelected={rect.id === selectedId}
+              onSelect={() => {
+                selectShape(rect.id);
+              }}
+              onChange={(newAttrs) => {
+                const rects = rectangles.slice();
+                rects[i] = newAttrs;
+                setRectangles(rects);
+              }}
+            />
+          ))}
+        </Layer>
+      </Stage>
+      <button onClick={handleAddRect}>Add Rectangle</button>
+      {selectedId && (
+        <div>
+          <h2>Selected Rectangle ID: {selectedId}</h2>
+          <pre>
+            {JSON.stringify(
+              rectangles.find((r) => r.id === selectedId),
+              null,
+              2
+            )}
+          </pre>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/KonvaCanvas.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Stage, Layer, Rect, Transformer } from "react-konva";
+
+const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  return (
+    <>
+      <Rect
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // so we have to manually correct its width and height
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && <Transformer ref={trRef} />}
+    </>
+  );
+};
+
+export default Rectangle;
